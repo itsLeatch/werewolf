fe02bbb4c9c9058dad95da2bf4f177efd09b018e
Ernests Mikuts <77016061+ErnGusMik@users.noreply.github.com>
2026-01-06 02:42:43 +0000

    Starter for connection with ARI library

--- asteriskHelper.py
+++ asteriskHelper.py
@@ -1,3 +1,69 @@
+import datetime
+import anyio
+import asyncari
+from asyncari.state import ToplevelChannelState, DTMFHandler
+
+# --- Configuration ---
+ARI_URL = "http://localhost:8088/"
+ARI_USERNAME = "asterisk"
+ARI_PASSWORD = "asterisk"
+APP_NAME = "hello" # Must match the Stasis() app name in extensions.conf
+
+clients = []
+
+class Connection:
+    def __init__(self, channel):
+        self.channel = channel
+        self.id = channel.id
+        self.vote = None
+        self.bridge_id = None
+        self.is_muted = False
+        self.is_connected = True
+        self.join_time = None
+        self.data = {}
+
+class HelloState(ToplevelChannelState, DTMFHandler):
+
+    # Runs on every new connection
+    async def on_start(self, channel):
+        print(f"New user connected! Channel: {self.channel_id}")
+        await self.channel.play(media="sound:hello-world")
+        player = Connection(channel)
+        player.join_time = datetime.now()
+        clients.append(player)
+
+    async def on_dtmf(self, event):
+        print(f"Digit {event.digit} pressed on channel {self.channel_id}")
+        player = next((p for p in clients if p.id == self.channel_id), None)
+        if not player:
+            return
+        player.vote = event.digit
+
+async def event_listener(client):
+    """The task that listens to events as long as the code runs"""
+    async with client.on_channel_event('StasisStart') as listener:
+        async for objs, event in listener:
+            channel = objs.get('channel')
+            if channel:
+                print("New channel joining!")
+                client.taskgroup.start_soon(HelloState(channel).start_task)
+        
+async def main():
+    """Runs the main event loop"""
+
+    async with asyncari.connect(
+        base_url=ARI_URL,
+        apps=APP_NAME,
+        username=ARI_USERNAME,
+        password=ARI_PASSWORD
+    ) as client:
+        client.taskgroup.start_soon(event_listener, client)
+        print("App starting soon!")
+
+        async for event in client:
+            print(f'Global event received: {event}')
+            print("Use this event for anything central, that needs more than one channel!")
+
 
 def playAudio(audioName):
     #print("Trying to play an audio, but it is not working, because the function was not implemented yet")
@@ -20,3 +86,14 @@ def getUserInput(playerNumber):
 
 def kickPlayer(playerNumber):
     print(f"Try to kick player {playerNumber}, but not implemented function!")
+
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        anyio.run(main)
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt: shutting down')
